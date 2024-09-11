8ffe42861aa885d5163a2c138f17c51b60cf00a4
Anton_Wombat <153746663+Anton119@users.noreply.github.com>
2024-09-11 20:54:15 +0300

    task from stepik
    У вас есть переменная A, которая содержит входные пользовательские данные.
    
    Напишите код, который удаляет все четные числа из множества A и записывает отсортированный результат в порядке возрастания в переменную result.
    
    Sample Input:
    
    [1, 2, 3, 4, 5, 6, 7]
    Sample Output:
    
    1, 3, 5, 7

---
+++ simple_sets4.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	//"sort"
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	A := ReadInput()
+	var result string
+	var B []int
+	var str []string
+	for i := 0; i < len(A); i++ {
+		if A[i]%2 != 0 {
+			B = append(B, A[i])
+		}
+	}
+	for i := 0; i < len(B)-1; i++ {
+		for j := 0; j < len(B)-1; j++ {
+			if B[j] > B[j+1] {
+				m := B[j+1]
+				B[j+1] = B[j]
+				B[j] = m
+			}
+		}
+	}
+	for i := 0; i < len(B); i++ {
+		str = append(str, strconv.Itoa(B[i]))
+	}
+
+	result = strings.Join(str, ", ")
+	fmt.Println(result)
+}
+
+func ReadInput() []int {
+	var A []int
+	var input string
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input = scanner.Text()
+	json.Unmarshal([]byte(input), &A)
+	return A
+}
